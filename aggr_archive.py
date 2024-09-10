5bede758b2ee0f70a132c5ccf7f235f4562cfa40
Sophie Buurman <nld1247@ac6-101.bullx>
2024-09-10 18:31:29 +0000

    removed all files

--- aggr_archive.py
+++
@@ -1,7 +0,0 @@
-"""
-Used to aggregate the statistics of a single archive which terminated before aggregation.
-"""
-
-from tozarr import ToZarr
-tz = ToZarr()
-tz.aggregate()
--- errors.py
+++
@@ -1,6 +0,0 @@
-# TODO : implement exception classes
-
-class ArrayNotFoundError(Exception):
-    def __init__(self, message="A custom error occurred"):
-        self.message = message
-        super().__init__(self.message)
